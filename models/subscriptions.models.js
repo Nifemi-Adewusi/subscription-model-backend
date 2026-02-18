7bb29743cdff31856209d4e2c2041d5eb04289a8
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-02-18 15:38:45 +0100

    Include the subscription model schema that captures the needed data for individual subscriptions

---
+++ models/subscriptions.models.js
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+  {
+    subscriptionName: {
+      type: String,
+      trim: true,
+      required: [true, "Subscription Name Is Required"],
+      minLength: 3,
+      maxLength: 30,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price Is Required"],
+      min: [0, "Price Must Be Greater Than 0"],
+    },
+    currency: {
+      type: String,
+      required: [true, "Currency Is Required"],
+      enum: ["NGN", "USD", "EUR"],
+    },
+    duration: {
+      type: Number,
+      required: [true, "Duration Is Required"],
+      min: [1, "Duration Must Be Greater Than 0"],
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+    },
+    category: {
+      type: String,
+      enum: ["food", "transportation", "entertainment", "utilities", "other"],
+      default: "other",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive", "expired"],
+      default: "active",
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= Date.now(),
+        message: "Start Date Must Be In The Past",
+      },
+    },
+    endDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          // This keyword refers to the object calling the method
+          return value > this.startDate;
+        },
+        message: "End Date Must Be Greater Than Start Date",
+      },
+    },
+  },
+  { timestamps: true },
+);
+
+const subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export default subscription;
