2f555d7dabf4cc3eba4b53f5762372ba2f9cd133
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-02-19 16:12:22 +0100

    Update the error handling of the middleware

---
+++ middleware/error.middleware.js
@@ -0,0 +1,14 @@
+const errorMiddleware = (err, req, res, next) => {
+  try {
+    let error = { ...err };
+    error.message = err.message;
+    console.error(err);
+
+    // Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      const message = "Resource not found";
+      error = new Error(message);
+      error.statusCode = 404;
+    }
+  } catch (err) {}
+};
