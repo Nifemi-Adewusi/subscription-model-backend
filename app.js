25e547e357fd6ebc0cf1867dc0818e2aedf7d531
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-02-20 01:35:25 +0100

    Update the app

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import subscriptionRouter from "./routes/subscription.routes.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middleware/error.middleware.js";
 
 const app = express();
 
@@ -17,9 +18,12 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+app.use(express.json());
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Subscription Tracker is active on http://localhost:${PORT}`);
