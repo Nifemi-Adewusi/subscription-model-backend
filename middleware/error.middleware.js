a126ce4f3b3ed0ceb403abf45e955029b06e2653
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-02-20 01:34:52 +0100

    Include all the needed status code and possible error handling

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -10,5 +10,25 @@ const errorMiddleware = (err, req, res, next) => {
       error = new Error(message);
       error.statusCode = 404;
     }
-  } catch (err) {}
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+      const message = "Duplicate field value entered";
+      error = new Error(message);
+      error.statusCode = 400;
+    }
+    // Mongose Validation error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((val) => val.message);
+      error = new Error(message.join(", "));
+      error.statusCode = 400;
+    }
+    res
+      .status(error.statusCode || 500)
+      .json({ success: false, error: error.message || "Server Error" });
+  } catch (error) {
+    next(error);
+  }
 };
+
+export default errorMiddleware;
