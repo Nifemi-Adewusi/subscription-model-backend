ebdbf7093eac3c02cf819777c7a41b848e3712ac
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-02-20 11:09:47 +0100

    Update the root to include the needed middlware from express

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -18,12 +19,19 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+// Allows Our Api to actually accept json coming from the clients
 app.use(express.json());
+
+// Allows our Api to accept form data
+app.use(express.urlencoded({ extended: false }));
+
+//Gets and exposes cookies from the request headers
+app.use(cookieParser());
+app.use(errorMiddleware());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
-app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
   console.log(`Subscription Tracker is active on http://localhost:${PORT}`);
