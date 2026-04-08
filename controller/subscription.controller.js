a57e1f010baadbe85f0cdde84fdd6607d4b255f9
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-04-08 13:04:17 +0100

    Update the subscription controller to inlude a controller to fetch all subscriptions for only authenticated users

--- controller/subscription.controller.js
+++ controller/subscription.controller.js
@@ -11,3 +11,34 @@ export const createSubscription = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getAllUserSubscriptions = async (_, res, next) => {
+  try {
+    const subscriptions = await Subscription.find();
+    res.status(200).json({
+      success: true,
+      message: "User subscriptions fetched successfully",
+      data: subscriptions,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserSubscription = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this subscription");
+      error.statusCode = 401;
+      throw error;
+    }
+    const subscription = await Subscription.find({ user: req.params.id });
+    res.status(200).json({
+      success: true,
+      message: "User subscription fetched successfully",
+      data: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
