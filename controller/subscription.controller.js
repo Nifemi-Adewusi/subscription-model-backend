cbd98f56f0c6b271f2c35bd29622be124f0d9770
Nifemi Adewusi <nifemiadewusi@gmail.com>
2026-04-08 11:24:22 +0100

    Add the subscription controller

---
+++ controller/subscription.controller.js
@@ -0,0 +1,13 @@
+import Subscription from "../models/subscriptions.models.js";
+
+export const createSubscription = async (req, res, next) => {
+  try {
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
+    });
+    res.status(201).json({ success: true, data: subscription });
+  } catch (error) {
+    next(error);
+  }
+};
